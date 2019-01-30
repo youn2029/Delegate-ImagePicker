60a6ea7f4c8750b0bd0ae38b96726899462e5ce8
윤성호 <youn2029@naver.com>
2019-01-30 15:47:38 +0900

    UIImagePickerController 실행

--- Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imgView: UIImageView! // 이미지뷰
 
+    // 이미지 선택 이벤트
+    @IBAction func pick(_ sender: Any) {
+        
+        // 이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary   // 이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 On
+        
+        // 이미지 피커 컨트롤러 실행
+        self.present(picker, animated: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
